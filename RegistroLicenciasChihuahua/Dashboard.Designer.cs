2e4ed021c5a41148ed7822df95ff41f5cdf3cade
Ximena Mendoza Villanueva <ximena13mendoza@gmail.com>
2022-05-23 12:58:07 -0600

    Usuario y rol

--- RegistroLicenciasChihuahua/Dashboard.Designer.cs
+++ RegistroLicenciasChihuahua/Dashboard.Designer.cs
@@ -31,9 +31,9 @@ namespace RegistroLicenciasChihuahua
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbl_Rol = new System.Windows.Forms.Label();
+            this.lbl_UserName = new System.Windows.Forms.Label();
             this.btn_cerrarsesion = new System.Windows.Forms.Button();
-            this.btn_User = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btn_mistramites = new System.Windows.Forms.Button();
@@ -42,21 +42,23 @@ namespace RegistroLicenciasChihuahua
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.lbl_UserName = new System.Windows.Forms.Label();
-            this.lbl_Rol = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.mainpanel.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.lbl_Rol);
-            this.panel1.Controls.Add(this.lbl_UserName);
             this.panel1.Controls.Add(this.tableLayoutPanel2);
+            this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.btn_mistramites);
@@ -67,20 +69,27 @@ namespace RegistroLicenciasChihuahua
             this.panel1.Size = new System.Drawing.Size(234, 624);
             this.panel1.TabIndex = 0;
             // 
-            // tableLayoutPanel2
+            // lbl_Rol
             // 
-            this.tableLayoutPanel2.ColumnCount = 1;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Controls.Add(this.btn_cerrarsesion, 0, 1);
-            this.tableLayoutPanel2.Controls.Add(this.btn_User, 0, 0);
-            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 554);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 51F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(232, 70);
-            this.tableLayoutPanel2.TabIndex = 4;
+            this.lbl_Rol.AutoSize = true;
+            this.lbl_Rol.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Rol.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(48)))), ((int)(((byte)(138)))));
+            this.lbl_Rol.Location = new System.Drawing.Point(69, 24);
+            this.lbl_Rol.Name = "lbl_Rol";
+            this.lbl_Rol.Size = new System.Drawing.Size(29, 16);
+            this.lbl_Rol.TabIndex = 6;
+            this.lbl_Rol.Text = "Rol";
+            // 
+            // lbl_UserName
+            // 
+            this.lbl_UserName.AutoSize = true;
+            this.lbl_UserName.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(48)))), ((int)(((byte)(138)))));
+            this.lbl_UserName.Location = new System.Drawing.Point(69, 0);
+            this.lbl_UserName.Name = "lbl_UserName";
+            this.lbl_UserName.Size = new System.Drawing.Size(73, 16);
+            this.lbl_UserName.TabIndex = 5;
+            this.lbl_UserName.Text = "UserName";
             // 
             // btn_cerrarsesion
             // 
@@ -89,27 +98,14 @@ namespace RegistroLicenciasChihuahua
             this.btn_cerrarsesion.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_cerrarsesion.ForeColor = System.Drawing.Color.White;
             this.btn_cerrarsesion.Image = ((System.Drawing.Image)(resources.GetObject("btn_cerrarsesion.Image")));
-            this.btn_cerrarsesion.Location = new System.Drawing.Point(3, 22);
+            this.btn_cerrarsesion.Location = new System.Drawing.Point(0, 0);
             this.btn_cerrarsesion.Name = "btn_cerrarsesion";
-            this.btn_cerrarsesion.Size = new System.Drawing.Size(226, 45);
+            this.btn_cerrarsesion.Size = new System.Drawing.Size(232, 49);
             this.btn_cerrarsesion.TabIndex = 0;
             this.btn_cerrarsesion.Text = "Cerrar sesión";
             this.btn_cerrarsesion.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btn_cerrarsesion.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btn_cerrarsesion.UseVisualStyleBackColor = false;
-            // 
-            // btn_User
-            // 
-            this.btn_User.AutoSize = true;
-            this.btn_User.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn_User.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_User.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(48)))), ((int)(((byte)(138)))));
-            this.btn_User.Location = new System.Drawing.Point(3, 0);
-            this.btn_User.Name = "btn_User";
-            this.btn_User.Size = new System.Drawing.Size(226, 19);
-            this.btn_User.TabIndex = 1;
-            this.btn_User.Text = "Usuario";
-            this.btn_User.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // pictureBox1
             // 
@@ -212,23 +208,54 @@ namespace RegistroLicenciasChihuahua
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
-            // lbl_UserName
+            // panel3
             // 
-            this.lbl_UserName.AutoSize = true;
-            this.lbl_UserName.Location = new System.Drawing.Point(12, 458);
-            this.lbl_UserName.Name = "lbl_UserName";
-            this.lbl_UserName.Size = new System.Drawing.Size(57, 13);
-            this.lbl_UserName.TabIndex = 5;
-            this.lbl_UserName.Text = "UserName";
+            this.panel3.Controls.Add(this.btn_cerrarsesion);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 575);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(232, 49);
+            this.panel3.TabIndex = 7;
             // 
-            // lbl_Rol
+            // tableLayoutPanel2
             // 
-            this.lbl_Rol.AutoSize = true;
-            this.lbl_Rol.Location = new System.Drawing.Point(12, 492);
-            this.lbl_Rol.Name = "lbl_Rol";
-            this.lbl_Rol.Size = new System.Drawing.Size(23, 13);
-            this.lbl_Rol.TabIndex = 6;
-            this.lbl_Rol.Text = "Rol";
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.44828F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 71.55173F));
+            this.tableLayoutPanel2.Controls.Add(this.lbl_UserName, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.lbl_Rol, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 526);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(232, 49);
+            this.tableLayoutPanel2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(48)))), ((int)(((byte)(138)))));
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Usuario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(48)))), ((int)(((byte)(138)))));
+            this.label3.Location = new System.Drawing.Point(3, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Rol:";
             // 
             // Dashboard
             // 
@@ -244,14 +271,14 @@ namespace RegistroLicenciasChihuahua
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.tableLayoutPanel2.ResumeLayout(false);
-            this.tableLayoutPanel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.mainpanel.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -267,11 +294,13 @@ namespace RegistroLicenciasChihuahua
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.Button btn_cerrarsesion;
-        private System.Windows.Forms.Label btn_User;
         private System.Windows.Forms.Label lbl_Rol;
         private System.Windows.Forms.Label lbl_UserName;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
